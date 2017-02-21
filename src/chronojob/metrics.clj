6aadc03cbb7914d4d23f8c405d91794e291d8f85
Aleksandr Sher <aleksandrsher@gmail.com>
2017-02-21 14:26:49 +0300

    + verbose stats

--- src/chronojob/metrics.clj
+++ src/chronojob/metrics.clj
@@ -3,31 +3,47 @@
             [clojure.tools.logging :as log]
             [chronojob.sql :as sql]
             [defcomponent :refer [defcomponent]]
-            [chronojob.hikaricp :as hikaricp])
+            [chronojob.hikaricp :as hikaricp]
+            [clojure.java.jdbc :as jdbc])
   (:import [io.prometheus.client Gauge CollectorRegistry]))
+
+(defn set-gauge!
+  [gauge label new-value]
+  (.. gauge
+      (labels (into-array [label]))
+      (set new-value)))
+
+(def default-stats
+  {:status/pending 0
+   :status/redo 0
+   :status/failed 0
+   :status/completed 0})
 
 (defn collect
   [db metric]
-  (let [count (-> {:select [:%count.id]
-                   :from [:jobs]
-                   :where [:or
-                           [:= :status #sql/raw "'pending'"]
-                           [:= :status #sql/raw "'redo'"]]}
-                  (sql/query-one db)
-                  :count)]
-    (.set metric (double count))))
+  (let [stats (->>
+               "select status, count(1) from jobs group by status;"
+               (jdbc/query db)
+               (reduce (fn [r {:keys [status count]}] (assoc r status count)) {})
+               (merge default-stats))]
+    (doseq [[status counter] stats]
+      (set-gauge! metric (name status) (double counter)))))
 
 (defn metrics-process
   [registry db]
   (let [stopper (a/promise-chan)
         metric (.. Gauge build
-                   (name "chronojob_pending")
-                   (help "number of pending jobs")
+                   (name "chronojob_status")
+                   (help "number of jobs by status")
                    (register (:registry registry)))]
     (a/go-loop []
       (a/alt!
-        (a/timeout 5000) ([_] (try (collect db metric) (catch Exception e (log/error e)))
-                          (recur))
+        ;; Every 10s write stats
+        (a/timeout 10000) ([_]
+                           (try
+                             (collect db metric)
+                             (catch Exception e (log/error e)))
+                           (recur))
         stopper ([_])))
     (fn [] (a/close! stopper))))
 
